7db24247edb7b3587a236588b8189a1a3407e18c
psjoon33 <tjdwnsdl0303@gmail.com>
2020-11-27 01:34:53 +0900

    .

--- 제이쓴마스터/장르만들기.py
+++ 제이쓴마스터/장르만들기.py
@@ -1,6 +1,6 @@
 import json
 
-with open('abc.json', encoding='utf-8') as json_file:
+with open('movies.json', encoding='utf-8') as json_file:
     json_data = json.load(json_file)
     
 # print(json_data[0])
@@ -12,3 +12,4 @@ for movie in json_data:
 
 with open('wars.json', 'w', encoding='utf-8') as make_file:
     json.dump(new, make_file, ensure_ascii=False, indent="\t")
+

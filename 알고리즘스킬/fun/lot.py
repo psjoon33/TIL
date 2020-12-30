18518f65ab5eb504b0d09b51e80726337a078ff2
psjoon33 <tjdwnsdl0303@gmail.com>
2020-12-30 22:24:51 +0900

    .

--- 알고리즘스킬/fun/lot.py
+++ 알고리즘스킬/fun/lot.py
@@ -8,13 +8,13 @@ for i in range(942):
 result = 0
 for i in range(932):
     x = a[i]
-    y = a[i+1:i+931]
+    y = a[i+1:i+26]
     for z in y:
         cnt = 0
         for w in x:
             if w in z:
                 cnt += 1
-        if cnt >= 3:
+        if cnt >= 2:
             result += 1
             print(i)
             print(x)
@@ -23,4 +23,5 @@ for i in range(932):
             break
     
 print(result)
-    
+
+# 최근 있었던 25개를 봤을 때 최소 1번의 케이스에 한해서 2개 이상의 숫자가 겹친다.

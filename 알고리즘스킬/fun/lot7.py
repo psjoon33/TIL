ff61d7dd5c326a0c516dcffe4f53fff04bf86d16
psjoon33 <tjdwnsdl0303@gmail.com>
2021-01-01 19:59:03 +0900

    .

--- 알고리즘스킬/fun/lot7.py
+++ 알고리즘스킬/fun/lot7.py
@@ -18,5 +18,13 @@ for i in range(n):
                 cnt.append(w)
         if len(cnt) > 3:
             if tuple(cnt) in result:
-                
+                result[tuple(cnt)] += 1
+            else:
+                result[tuple(cnt)] = 1
+count = 0
+for r in result:
+    if result[r] >2:
+        count += 1
+print(count)
 
+# 4개 이상 겹치는 갯수가 2개 이상인 경우는 없다.

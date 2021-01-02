bc85323a2c84f3877b34d1e13e19e34dbf14e174
psjoon33 <tjdwnsdl0303@gmail.com>
2021-01-02 15:26:42 +0900

    .

--- 알고리즘스킬/fun/최종.py
+++ 알고리즘스킬/fun/최종.py
@@ -6,53 +6,48 @@ n = 942
 for i in range(n):
     a.append(list(map(int, input().split('\t'))))
 
-def check(lot):
+
+def check(x):
     # 25번 내에 겹치는 2개가 없다면 break
     global n
     result = '25번 내에 2개가 겹치는 경우가 없다,,'
-    for i in range(n-25):
-        x = a[i]
-        y = a[i+1:i+26]
-        for z in y:
-            cnt = 0
-            for w in x:
-                if w in z:
-                    cnt += 1
-            if cnt >= 2:
-                result = 'Yes'
-                break
-        if result == 'Yes':
+    y = a[0:26]
+    for z in y:
+        cnt = 0
+        for w in x:
+            if w in z:
+                cnt += 1
+        if cnt >= 2:
+            result = 'Yes'
             break
+    if result == 'Yes':
+        break
     print(result)
     # 이전 회차와 3개 이상 겹치면 break
     result = '이전 회차와 3개 이상 겹친다.'
-    for i in range(n-1):
-        x = a[i]
-        y = a[i+1]
-        cnt = 0
-        for w in x:
-            if w in y:
-                cnt += 1
-        if cnt < 3:
-            result = 'Yes' 
-            break
+    y = a[0]
+    cnt = 0
+    for w in x:
+        if w in y:
+            cnt += 1
+    if cnt < 3:
+        result = 'Yes' 
+        break
     print(result)
     # 2개 겹치는 경우가 최근 8개에서 2번 있으면 break
     result = 'Yes'
-    for i in range(n-8):
-        res = {}
-        x = a[i]
-        y = a[i+1:i+9]
-        for z in y:
-            inc = []
-            for w in x:
-                if w in z:
-                    inc.append(w)
-        if len(inc) > 1:
-            if tuple(inc) in res:
-                result[tuple(inc)] += 1
-            else:
-                result[tuple(inc)] = 1
+    res = {}
+    y = a[0:8]
+    for z in y:
+        inc = []
+        for w in x:
+            if w in z:
+                inc.append(w)
+    if len(inc) > 1:
+        if tuple(inc) in res:
+            res[tuple(inc)] += 1
+        else:
+            res[tuple(inc)] = 1
     for j in res:
         if res[j] > 1:
             result = '8번 중에 2개 이상 겹치는 경우 2번 이상'
@@ -60,33 +55,85 @@ def check(lot):
     print(result)
     # 80번 안에 2개 이상 겹치는 경우 2번 미만
     result = '80번 안에 2개 이상 겹치는 경우 2번 미만'
-    for i in range(n-80):
-        res = {}
-        x = a[i]
-        y = a[i+1:i+81]
-        for z in y:
-            inc = []
-            for w in x:
-                if w in z:
-                    inc.append(w)
-            if len(inc) > 1:
-                if tuple(inc) in res:
-                    res[tuple(inc)] += 1
-                else:
-                    res[tuple(inc)] = 1
-        for j in res:
-            if res[j] > 1:
-                result = 'Yes'
-                break
+    res = {}
+    y = a[0:80]
+    for z in y:
+        inc = []
+        for w in x:
+            if w in z:
+                inc.append(w)
+        if len(inc) > 1:
+            if tuple(inc) in res:
+                res[tuple(inc)] += 1
+            else:
+                res[tuple(inc)] = 1
+    for j in res:
+        if res[j] > 1:
+            result = 'Yes'
+            break
+    print(result)
+    # 120개를 보면 3개 이상 겹치는 경우가 무조건 있음
+    result = '120개에 3개 겹치는경우 없,,'
+    y = a[0:120]
+    for z in y:
+        cnt = 0
+        for w in x: 
+            if w in z:
+                cnt += 1
+        if cnt == 3:
+            result = 'Yes'
+            break
+    if result == 'Yes':
+        break
+    print(result)
+    # 50개를 봤을 때 4개 이상 겹치는 경우 없다.
+    result = 'Yes'
+    y = a[:50]
+    for z in y:
+        cnt = 0
+        for w in x:
+            if w in z:
+                cnt += 1
+        if cnt > 3:
+            result = '50개 중에 4개 이상 겹친다.'    
+            break
+    print(result)
+    # 4개 이상 겹치는 갯수가 2개 초과인 경우는 없다.
+    res = {}
+    result = 'Yes'
+    y = a[:]
+    for z in y:
+        if z == x:
+            continue
+        cnt = []
+        for w in x:
+            if w in z:
+                cnt.append(w)
+        if len(cnt) > 3:
+            if tuple(cnt) in res:
+                res[tuple(cnt)] += 1
+            else:
+                res[tuple(cnt)] = 1
+    count = 0
+    for r in res:
+        if res[r] > 2
+            result = '4개 이상 겹치는 경우가 2개 초과'
+            break
+    print(result)
+    # 전체에서 5개 이상 겹치는 경우 없다.
+    y = a
+    result = 'Yes'
+    for z in y:
+        cnt = 0
+        for w in x:
+            if w in z:
+                cnt += 1
+        if cnt > 4:
+            result = '5개 이상 같다.'
     print(result)
 
 
-
-
-
-
-
-
+put = []
 
 
 

0a25150df2ff0175396e0bf145712a9b99d3f30e
psjoon33 <tjdwnsdl0303@gmail.com>
2021-01-16 21:18:04 +0900

    .

--- 알고리즘스킬/fun/최종.py
+++ 알고리즘스킬/fun/최종.py
@@ -130,7 +130,7 @@ def check(x):
 
 import random
 put = sorted(random.sample(range(1, 46), 6))
-# put = [9, 10, 15, 30, 33, 37]
+# put = [9, 18, 19, 30, 34, 40]
 print(a[2])
 print(a[1])
 print(a[0])

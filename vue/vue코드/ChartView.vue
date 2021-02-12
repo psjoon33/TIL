b8c304b0baad26be6f574c69a51a91e9d1ec8605
psjoon33 <tjdwnsdl0303@gmail.com>
2021-02-12 21:20:16 +0900

    chart-making

---
+++ vue/vue코드/ChartView.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <div class="columns">
+      <div class="column" style="margin-left: 50px; margin-top: 100px">
+        <div style="width: 30vw; height: 50vh">
+          <p
+            style="
+              justify-content: center;
+              text-align: center;
+              font-size: 1.2em;
+            "
+          >
+            박성준님의 일주일간 칼로리 섭취 추이
+          </p>
+          <line-chart
+            style="width: 30vw; height: 40vh; min-height: 20px !important"
+          ></line-chart>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+
+<script>
+import LineChart from "@/components/LineChart";
+
+export default {
+  name: "VueChartJS",
+  components: {
+    LineChart,
+  },
+};
+</script>
---
+++ vue/vue코드/LineChart.vue
@@ -0,0 +1,60 @@
+<script>
+//Importing Line class from the vue-chartjs wrapper
+import { Line } from "vue-chartjs";
+//Exporting this so it can be used in other components
+export default {
+  extends: Line,
+  data() {
+    return {
+      datacollection: {
+        //Data to be represented on x-axis
+        labels: ["D-7", "D-6", "D-5", "D-4", "D-3", "D-2", "D-1"],
+        datasets: [
+          {
+            label: "칼로리 섭취량",
+            backgroundColor: "white",
+            pointBackgroundColor: "white",
+            borderWidth: 10,
+            pointBorderColor: "black",
+            //Data to be represented on y-axis
+            data: [40, 20, 30, 50, 90, 30, 60],
+            // padding-left: '20px'
+          },
+        ],
+      },
+      //Chart.js options that controls the appearance of the chart
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                padding: 50,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    //renderChart function renders the chart with the datacollection and options object.
+    this.renderChart(this.datacollection, this.options);
+  },
+};
+</script>

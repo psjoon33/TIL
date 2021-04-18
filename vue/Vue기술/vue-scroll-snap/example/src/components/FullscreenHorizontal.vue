a8acca936155076ffb99e1d6a665d1afe5ff965a
psjoon33 <tjdwnsdl0303@gmail.com>
2021-04-18 22:52:49 +0900

    vue-scroll-snap

---
+++ vue/Vue기술/vue-scroll-snap/example/src/components/FullscreenHorizontal.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-scroll-snap :fullscreen="true" :horizontal="true">
+    <div class="item" id="one"></div>
+    <div class="item" id="two">asdfadsf</div>
+    <div class="item" id="three">asdfdasf</div>
+    <div class="item" id="four">asdfasd</div>
+  </vue-scroll-snap>
+</template>
+
+<script>
+import VueScrollSnap from "vue-scroll-snap";
+
+export default {
+  name: "FullscreenHorizontal",
+  components: { VueScrollSnap },
+  data() {
+    return {};
+  },
+  methods: {
+    findy() {
+      console.log(window.scrollY);
+    },
+  },
+};
+</script>
---
+++ vue/Vue기술/vue-scroll-snap/example/src/components/FullscreenVertical.vue
@@ -0,0 +1,34 @@
+<template>
+  <vue-scroll-snap :fullscreen="true">
+    <div class="item" id="one"></div>
+    <div class="item" id="two"></div>
+    <div class="item" id="three"></div>
+    <div class="item" id="four"></div>
+  </vue-scroll-snap>
+</template>
+
+<script>
+import VueScrollSnap from "vue-scroll-snap";
+
+export default {
+  name: "FullscreenVertical",
+  components: { VueScrollSnap },
+  data() {
+    return {
+      windowTop: window.top.scrollY,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll(e) {
+      this.windowTop = e.target.scrollTop;
+      console.log(this.windowTop);
+    },
+  },
+};
+</script>
